62f3718f805427334b04a7ca770c63d4d66c46ae
mramos09-alt <m.ramos09@escola.pr.gov.br>
2026-05-26 09:07:08 -0300

    Add files via upload

---
+++ batalha.js
@@ -0,0 +1,11 @@
+function lutar(){
+    const classe = document.getElementById('classe').value;
+    const arma = document.getElementById('arma').value.trim();
+    const log= document.getElementById('log');
+
+    if(!arma){
+        log.innerHTML ="voce esqueceu de equipar uma arma"
+}
+
+
+}
